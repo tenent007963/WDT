fd6536fe432ebff6cb8816968f6b77e20a4b4dac
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-04-05 15:29:17 +0800

    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+include_once "./header.php"
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- title -->
+        <title>Alivestock Management System</title>
+        <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+        <!-- metadata -->
+        <meta property="og:title" content="Site Title" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="//wdt.svrcd.xyz" />
+        <meta property="og:image" content="//wdt.svrcd.xyz/images/logo.png" />
+        <meta property="og:description" content="Alivestock Management System is a online management platform for managing livestocks easily." />
+        <meta name="theme-color" content="#656565">
+        <meta name="twitter:card" content="summary_large_image">
+    </head>
+</html>
---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * A simple, clean and secure PHP Login Script / MINIMAL VERSION
+ *
+ * Uses PHP SESSIONS, modern password-hashing and salting and gives the basic functions a proper login system needs.
+ *
+ * @author Panique
+ * @link https://github.com/panique/php-login-minimal/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// include the configs / constants for the database connection
+require_once("config/db.php");
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) {
+    // the user is logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are logged in" view.
+    include("views/logged_in.php");
+
+} else {
+    // the user is not logged in. you can do whatever you want here.
+    // for demonstration purposes, we simply show the "you are not logged in" view.
+    include("views/not_logged_in.php");
+}
