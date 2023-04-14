4f6b896d6dde27e535ee172a1d23ee7111fec61c
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-04-14 11:49:09 +0800

    rough code

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?=$title?> - InLine Scheduling System</title>
+        <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+        <meta property="og:title" content="InLine Scheduling System" />
+        <meta property="og:type" content="website, scheduling, queue, system" />
+        <meta property="og:url" content="//wdt.svrcd.xyz" />
+        <meta property="og:image" content="//wdt.svrcd.xyz/images/logo.png" />
+        <meta property="og:description" content="InLine Scheduling System is an online platform for scheduling appointments easily." />
+        <meta name="theme-color" content="#656565">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
--- index.php
+++ index.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <!-- title -->
-        <title>InLine Scheduling System</title>
-        <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
-        <!-- metadata -->
-        <meta property="og:title" content="InLine Scheduling System" />
-        <meta property="og:type" content="website, scheduling, queue, system" />
-        <meta property="og:url" content="//wdt.svrcd.xyz" />
-        <meta property="og:image" content="//wdt.svrcd.xyz/images/logo.png" />
-        <meta property="og:description" content="InLine Scheduling System is an online platform for scheduling appointments easily." />
-        <meta name="theme-color" content="#656565">
-        <meta name="twitter:card" content="summary_large_image">
-    </head>
-    <body>
-    <header><?php include_once "./header.php" ?></header>
+<?php 
+$title = "Home";
+include_once "./header.php"; 
+echo "<body>"
+include_once "./navbar.php";
+echo "<span>This is a test page</span>"
 
-    <footer><?php include_once "./footer.php" ?></footer>
-    </body>
-</html>
+
+include_once "./footer.php" 
+echo "</body> </html>"
+?>
