bf95666272c87a251f40dc12fa5cde0c8562197e
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-04-15 11:19:33 +0800

    run and test

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php 
 $title = "Home";
-include_once "./header.php"; 
-echo "<body>"
+include_once "./header.php";
+echo "<body>";
 include_once "./navbar.php";
-echo "<span>This is a test page</span>"
+echo "<span>This is a test page</span>";
 
 
-include_once "./footer.php" 
-echo "</body> </html>"
+include_once "./footer.php" ;
+echo "</body> </html>";
 ?>
