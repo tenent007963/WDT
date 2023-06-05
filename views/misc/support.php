a78a9056a16ab436c40cda332bb638e678378677
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-05 19:55:23 +0800

    support.php

---
+++ views/misc/support.php
@@ -0,0 +1,16 @@
+<div class="help" id="faq">
+    <h3>1. Why this website looks so ugly?</h3>
+    <span>The website is made oriented to elders, hence we want to make it as simple as possible instead of having fancy animations or graphics.</span>
+    <br />
+    <h3>2. How long it takes to create this website?</h3>
+    <span>1 bloody week.</span>
+    <br />
+    <h3>3. How do I perform tasks, eg: creating a new appointment?</h3>
+    <span>Very easy, just login to the portal then select your desired actions from the left menu, the page will load at right side and everything is straight-forward after this point.</span>
+    <br />
+    <h3>4. Can I register account as technician?</h3>
+    <span>Hell nah</span>
+    <br />
+    <h3>5. How do I contact for more support?</h3>
+    <span>You can email us thru wdt@svrcd.xyz</span>
+</div>
