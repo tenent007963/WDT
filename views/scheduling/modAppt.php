82506bd4bef22240f41915729f4823e622910112
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-10 12:41:40 +0800

    modification&sync

--- views/scheduling/modAppt.php
+++ views/scheduling/modAppt.php
@@ -31,89 +31,87 @@ $raw_data1 = $db_connection->query($sql1);
 if (($raw_data1->num_rows == 1) && ($raw_data->num_rows > 0) ){
     $options = mysqli_fetch_all($raw_data, MYSQLI_ASSOC);
     $data = mysqli_fetch_all($raw_data1, MYSQLI_ASSOC);
+    ?>
+    <form class="form-horizontal">
+    <fieldset>
+
+    <!-- Form Name -->
+    <legend>Modify Appointment</legend>
+
+    <!-- Select Basic -->
+    <div class="form-group">
+        <label class="col-md-4 control-label" for="sche_id">Appointment ID</label>
+        <div class="col-md-4">
+            <input id="sche_id" name="sche_id" type="text" value="<?=$data['sche_id']?>" class="form-control input-md" disabled>
+                
+        </div>
+    </div>
+
+    <!-- Select Basic -->
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="symp_is">Symptom</label>
+    <div class="col-md-4">
+        <select id="symp_is" name="symp_is" class="form-control">
+            <?php
+            foreach ($options as $option) { ?>
+                <option value="<?=$option['symp_id']?>" <?=($option['symp_id']==$data['symp_id']) ? "selected" : ''?> ><?=$option['symp_name']?></option>
+            <?php } ?>
+        </select>
+    </div>
+    </div>
+
+    <!-- Text input-->
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="date1">Appointment Date</label>
+    <div class="col-md-4">
+    <input id="date1" name="date1" type="date" class="form-control input-md" required value='<?=$data['date1']?>'>
+        
+    </div>
+    </div>
+
+    <!-- Text input-->
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="time1">Preferred Time Slot</label>
+    <div class="col-md-4">
+    <input id="time1" name="time1" type="time" list="slots" class="form-control input-md" required>
+    <datalist id="slots">
+        <option value="08:00">
+        <option value="09:00">
+        <option value="10:00">
+        <option value="11:00">
+        <option value="12:00">
+        <option value="13:00">
+        <option value="14:00">
+        <option value="15:00">
+        <option value="16:00">
+        <option value="17:00">
+        <option value="18:00">
+        <option value="19:00">
+        <option value="20:00">
+    </datalist>
+    </div>
+    </div>
+
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="cust_cmt">Customer Comment</label>
+    <div class="col-md-4">
+        <textarea class="form-control" id="cust_cmt" name="cust_cmt"><?=$data['cust_cmt']?></textarea>
+    </div>
+    </div>
+
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="submit">Update Details?</label>
+    <div class="col-md-4">
+        <button id="submit" name="submit" class="btn btn-primary">Yes</button>
+    </div>
+    </div>
+
+    </fieldset>
+    </form>
+<?php
 } else {
     echo "<h2>Something went wrong, please try again.</h2>";
 }
 
-?>
-
-<form class="form-horizontal">
-<fieldset>
-
-<!-- Form Name -->
-<legend>Modify Appointment</legend>
-
-<!-- Select Basic -->
-<div class="form-group">
-    <label class="col-md-4 control-label" for="sche_id">Appointment ID</label>
-    <div class="col-md-4">
-        <input id="sche_id" name="sche_id" type="text" value="<?=$data['sche_id']?>" class="form-control input-md" disabled>
-            
-    </div>
-</div>
-
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="symp_is">Symptom</label>
-  <div class="col-md-4">
-    <select id="symp_is" name="symp_is" class="form-control">
-        <?php
-        foreach ($options as $option) { ?>
-            <option value="<?=$option['symp_id']?>" <?=($option['symp_id']==$data['symp_id']) ? "selected" : ''?> ><?=$option['symp_name']?></option>
-        <?php } ?>
-    </select>
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="date1">Appointment Date</label>
-  <div class="col-md-4">
-  <input id="date1" name="date1" type="date" class="form-control input-md" required value='<?=$data['date1']?>'>
-    
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="time1">Preferred Time Slot</label>
-  <div class="col-md-4">
-  <input id="time1" name="time1" type="time" list="slots" class="form-control input-md" required>
-  <datalist id="slots">
-      <option value="08:00">
-      <option value="09:00">
-      <option value="10:00">
-      <option value="11:00">
-      <option value="12:00">
-      <option value="13:00">
-      <option value="14:00">
-      <option value="15:00">
-      <option value="16:00">
-      <option value="17:00">
-      <option value="18:00">
-      <option value="19:00">
-      <option value="20:00">
-  </datalist>
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="cust_cmt">Customer Comment</label>
-  <div class="col-md-4">
-    <textarea class="form-control" id="cust_cmt" name="cust_cmt"><?=$data['cust_cmt']?></textarea>
-  </div>
-</div>
-
-<div class="form-group">
-  <label class="col-md-4 control-label" for="submit">Update Details?</label>
-  <div class="col-md-4">
-    <button id="submit" name="submit" class="btn btn-primary">Yes</button>
-  </div>
-</div>
-
-</fieldset>
-</form>
-
-<?php
  $db_connection -> close();
 ?>
