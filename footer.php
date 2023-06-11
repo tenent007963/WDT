026a395398d5316676bf4529011e5bbc07df0a36
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-11 18:02:04 +0800

    fkin jquery

--- footer.php
+++ footer.php
@@ -44,5 +44,4 @@
     <?php } ?>
     </footer>
     </body>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 </html>
