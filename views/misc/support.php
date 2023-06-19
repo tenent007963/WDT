828ddf77c5b29f93b51508b3ba042c3e0f8c9f89
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2023-06-19 18:05:13 +0800

    updatte support cintents

--- views/misc/support.php
+++ views/misc/support.php
@@ -1,16 +1,21 @@
 <div class="help" id="faq">
-    <h3>1. Why this website looks so ugly?</h3>
+    <h3>1. Why does the website design look simple?</h3>
     <span>The website is made oriented to elders, hence we want to make it as simple as possible instead of having fancy animations or graphics.</span>
     <br />
-    <h3>2. How long it takes to create this website?</h3>
-    <span>1 bloody week.</span>
+    <h3>2. What is this website?</h3>
+    <span>This is an appointment scheduling website to schedule websites with our establishment.</span>
     <br />
-    <h3>3. How do I perform tasks, eg: creating a new appointment?</h3>
-    <span>Very easy, just login to the portal then select your desired actions from the left menu, the page will load at right side and everything is straight-forward after this point.</span>
+    <h3>3. What functions can I do as a customer?</h3>
+    <span>Your options are to check, modify, or book your appointments. You could modify your account details too.</span>
     <br />
-    <h3>4. Can I register account as technician?</h3>
-    <span>Hell nah</span>
+    <h3>4. How do I cancel an appointment?</h3>
+    <span>It is not possible to do so as a customer. Please contact your technician in the event of change of mind.</span>
     <br />
-    <h3>5. How do I contact for more support?</h3>
-    <span>You can email us thru wdt@svrcd.xyz</span>
+    <h3>5. Can I register as a technician?</h3>
+    <span>No, you will have to contact our administrators so they can determine if you can be hired and subsequently added into our system.</span>
+    <br />
+    <h3>6. How do I contact staff support?</h3>
+    <span>You can reach us via our social media links or email us through wdt@svrcd.xyz.</span>
+    <br />
+    <h2>Other FAQs and answers will be updated here in the future.</h3>
 </div>
